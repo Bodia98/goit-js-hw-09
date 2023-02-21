e48669dd536653ab409865d935db0854853d283b
Bodia98 <stefanyuk1998@gmail.com>
2023-02-21 11:09:50 +0200

    finish

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,48 @@
+import Notiflix from 'notiflix';
+
+const refs = {
+  inputDelay: document.querySelector('input[name="delay"]'),
+  inputStep: document.querySelector('input[name="step"]'),
+  inputAmount: document.querySelector('input[name="amount"]'),
+  form: document.querySelector('form'),
+};
+
+refs.form.addEventListener('submit', e => {
+  e.preventDefault();
+  if (
+    Number.parseInt(refs.inputDelay.value) < 0 ||
+    Number.parseInt(refs.inputAmount.value) < 0 ||
+    Number.parseInt(refs.inputStep.value) < 0
+  ) {
+    Notiflix.Notify.failure('Error! Your values is negative!');
+    return;
+  }
+  let delayValue = Number.parseInt(refs.inputDelay.value);
+  for (let i = 1; i <= Number.parseInt(refs.inputAmount.value); i += 1) {
+    createPromise(Number.parseInt(i), delayValue)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(
+          `✅ Fulfilled promise ${position} in ${delay}ms`
+        );
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(
+          `❌ Rejected promise ${position} in ${delay}ms`
+        );
+      });
+    delayValue += Number.parseInt(refs.inputStep.value);
+  }
+});
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
